668479a5e21298497bc6c3ad2d7f37798078e9ed
Guillaume Marchand <gmarchan@amazon.fr>
2023-03-21 14:58:31 +0100

    fix

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 setup(name='pyawshelpers',
       version='0.1',
       description='Python helper classes and functions for AWS API',
-      url='http://github.com/gmarchand/py-aws-helpers',
+      url='http://github.com/gmarchand/pyawshelpers',
       author='Guillaume Marchand',
-      author_email='contact@amazon.com',
+      author_email='gmarchan@amazon.com',
       classifiers=[
           'Development Status :: 4 - Beta',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.9'
       ],
-      packages=['awshelpers'],
+      packages=['pyawshelpers'],
       zip_safe=False,
       install_requires=['boto3','progress'])
